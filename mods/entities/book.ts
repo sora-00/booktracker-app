68ec9d841d2b6140bb5a3956eac24185d5c2581c
sora-00 <sora.umiushi@gmail.com>
2025-11-16 18:40:16 +0900

    feat:bookエンティティの定義

--- mods/entities/book.ts
+++ mods/entities/book.ts
@@ -1,9 +1,17 @@
+import type { Status } from "./status";
+
 export type Book = {
   id: number;
   title: string;
   author: string;
+  totalPages: number;
+  publisher: string;
+  background?: string;
   thumbnailUrl?: string;
-  description?: string;
+  status: Status;
+  targetCompleteDate: string;
+  completedPages: number;
+  targetPagesPerDay: number;
   createdAt: string; // ISO8601
   updatedAt: string; // ISO8601
 };
@@ -11,16 +19,28 @@ export type Book = {
 export type NewBookInput = {
   title: string;
   author: string;
+  totalPages: number;
+  publisher: string;
+  background?: string;
   thumbnailUrl?: string;
-  description?: string;
+  status: Status;
+  targetCompleteDate: string;
+  completedPages: number;
+  targetPagesPerDay: number;
 };
 
 export const createBook = (id: number, input: NewBookInput, nowIso: string): Book => ({
   id,
   title: input.title,
   author: input.author,
+  totalPages: input.totalPages,
+  publisher: input.publisher,
+  background: input.background,
   thumbnailUrl: input.thumbnailUrl,
-  description: input.description,
+  status: input.status,
+  targetCompleteDate: input.targetCompleteDate,
+  completedPages: input.completedPages,
+  targetPagesPerDay: input.targetPagesPerDay,
   createdAt: nowIso,
   updatedAt: nowIso,
 });
